3f15ef7578efdfbc78ec306e5e050b8abb78148b
Pedro Del Claro <pdelclarol@gmail.com>
2022-03-07 22:44:29 -0300

    add coments to game() and correct mispelling

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Rock, Paper, Scissor
+# Rock, Paper, Scissors
 
-This is a simple rock, paper, scissor game built as a project for The Odin Project's Javascript Foundations course.
+This is a simple rock, paper, scissors game built as a project for The Odin Project's Javascript Foundations course.
 
 Initially, this game will be set up to be played only from the console, but, in the next steps, a GUI will be implemented.
 
--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-console.log("HelloWorld")
-
 // When the user makes a selection
-
 // Make the player's selection case-insensitive and stores in a variable called playerSelection
+// script.js:9 Uncaught TypeError: Cannot read properties of null (reading 'toLowerCase')
 
 const playerSelection = playerPlay();
 function playerPlay() {
-   return window.prompt("Rock, Paper or Scissor?", "").toLowerCase();
+   return window.prompt("Rock, Paper or Scissors?", "").toLowerCase();
 }
 console.log(playerSelection)
 
@@ -14,36 +12,26 @@ console.log(playerSelection)
 
 const computerSelection = computerPlay();
 function computerPlay() {
-    const choice = ["rock", "paper", "scissor"];
+    const choice = ["rock", "paper", "scissors"];
     return choice[Math.floor(Math.random() * choice.length)];
 }
 console.log(computerSelection);
 
 // Make a function called playRound with playerSelection and computerSelection as parameters that plays a round and returns the result
-/* se pS = scissor and cS = paper print You win! Scissor beats Paper!
-   se pS = paper and cS = rock print You win, paper beats Rock!
-   se pS = rock and cS = scissor print You win! Rock beats Scissor.
-   se pS = scissor and cS = rock print You lose! Rock beats Scissor!
-   se pS = rock and cS = paper print You lose! Paper beats Rock!
-   se pS = paper and cS = scissor print You lose! Scissor beats Paper!
-   Se ps = cS print It'sa tie
-   else prints something wrong
-*/
-
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === "scissor" && computerSelection === "paper") {
-        return "You win! Scissor beats paper."
+    if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You win! Scissors beats paper."
     } else if (playerSelection === "paper" && computerSelection === "rock") {
         return "You win! Paper beats rock."
-    } else if (playerSelection === "rock" && computerSelection === "scissor") {
-        return "You win! Rock beats scissor."
-    } else if (playerSelection === "scissor" && computerSelection === "rock") {
-        return "You lose! Rock beats scissor."
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You win! Rock beats scissors."
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You lose! Rock beats scissors."
     } else if (playerSelection === "rock" && computerSelection === "paper") {
         return "You lose! Paper beats rock."
-    } else if (playerSelection === "paper" && computerSelection === "scissor") {
-        return "You lose! Scissor beats paper."
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You lose! Scissors beats paper."
     } else if (playerSelection === computerSelection) {
         return "It's a tie! Try again."
     } else {
@@ -53,3 +41,17 @@ function playRound(playerSelection, computerSelection) {
 console.log(playRound(playerSelection, computerSelection));
 
 // Make a function called game that uses loop, calss plaRound to play a 5 round game, keeps score and returns a winner
+/* Criar uma variavel para armazenar o score do jogador = 0
+Criar uma variavel para armazenar o score do pc = 0
+Cria uma variavel game com um loop que:
+    Por 5 vezes:
+        Joga um round
+        console.log(playRound(playerSelection, computerSelection));
+        Se playRound === .startWith(You win) add +1 player score
+        Caso contrário add +1 computer score
+    Após a 5 vez:
+        Compara o score do Player com o do Computer
+        Se Player > Computer print You Win. The score is {Player} vs. {Computer}
+        Caso contrário > YOu lose
+        Restart?        
+*/
