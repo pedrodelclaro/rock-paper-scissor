adb4b145c30ad83ba9e9c4cf08ce4224b3308bb9
Pedro Del Claro <pdelclarol@gmail.com>
2022-03-08 23:09:30 -0300

    remove pseudocode

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-// When the user makes a selection
-// Make the player's selection case-insensitive
+// Gets player's selection and makes it case-insensitive
 
 function playerPlay() {
     const playerInput = prompt("Rock, Paper or Scissors?", "") + "";
     return playerInput.toLowerCase();
 }
 
-// Make a funcition called computerPlay that Random picks between Rock Paper Scissor and store in a variable called computerSelection
+// Make computer picks randomly between Rock, Paper or Scissors
 
 function computerPlay() {
     const choice = ["rock", "paper", "scissors"];
     return choice[Math.floor(Math.random() * choice.length)];
 }
 
-// Make a function called playRound with playerSelection and computerSelection as parameters that plays a round and returns the result
+// Play a round and return the winner
 
 function playRound() {
     const playerSelection = playerPlay();
@@ -39,22 +38,7 @@ function playRound() {
     }
 }
 
-// Make a function called game that uses loop, calss plaRound to play a 5 round game, keeps score and returns a winner
-/* Criar uma variavel para armazenar o score do jogador = 0
-Criar uma variavel para armazenar o score do pc = 0
-Cria uma variavel game com um loop que:
-    Por 5 vezes:
-        Joga um round
-        console.log(playRound(playerSelection, computerSelection));
-        Se playRound === .startWith(You win) add +1 player score
-        Caso contrário add +1 computer score
-    Após a 5 vez:
-        Compara o score do Player com o do Computer
-        Se Player > Computer print You Win. The score is {Player} vs. {Computer}
-        Caso contrário > YOu lose
-        Restart?        
-*/
-
+// Plays a 5 round game, keeping score and reporting the winner
 
 let playerScore = 0;
 let computerScore = 0;
